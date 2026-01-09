a1a0a33a089eef1cf2508292aec389633b466817
Klimentev Andrey <klmntv27@gmail.com>
2026-01-09 17:19:58 +0300

    update: Sessions table returned

--- src/database/migrations/0001_01_01_000000_create_users_table.php
+++ src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,10 +16,20 @@ return new class extends Migration
             $table->string('last_name')->nullable();
             $table->integer('currency');
         });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('sessions');
     }
 };
